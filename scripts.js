0a56089543476d58e5574c692394103337ad789d
Daniil Russkikh <evilrococo@gmail.com>
2025-03-24 00:08:43 +0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+function test() {
+  new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        enabled: true,
+      },
+    },
+  });
+}
+
+this.test();
+
+let button = document.querySelector(".show-button");
+let background = document.querySelector(".show-button--img");
+let text = document.getElementById("show-button--text");
+button.addEventListener("click", function () {
+  button.classList.toggle("active");
+  if (button.classList.contains("active")) {
+    text.textContent = "Скрыть";
+    background.classList.remove("show-button--img");
+    background.classList.add("show-button--img--rotate");
+  } else {
+    text.textContent = "Показать все";
+    background.classList.remove("show-button--img--rotate");
+    background.classList.add("show-button--img");
+  }
+});
+
+let repairingList = document.querySelectorAll(".repairing-list")[0].children;
+let button1 = document.querySelector(".show-button");
+console.log(repairingList);
+button1.addEventListener("click", function () {
+  for (let i = 0; i <= repairingList.length; i++) {
+    if (i > 5 && window.innerWidth < 1120) {
+      repairingList[i].classList.toggle("hidden");
+    } else if (i > 7 && window.innerWidth > 1120) {
+      repairingList[i].classList.toggle("hidden");
+    }
+  }
+});
+
+for (let i = 0; i <= repairingList.length; i++) {
+  if (i > 5 && window.innerWidth < 1120) {
+    repairingList[i].classList.toggle("hidden");
+  } 
+}
